d84ce31fe41845a1e081da09400311e0ef00e603
ryouka <coolblossom.0130@gmail.com>
2018-03-19 07:54:38 +0900

    ArrayDescriptorのテストケース追加

--- java/src/test/java/net/coolblossom/lycee/core/args/descriptors/ArrayDescriptorTest.java
+++ java/src/test/java/net/coolblossom/lycee/core/args/descriptors/ArrayDescriptorTest.java
@@ -1,81 +1,78 @@
 package net.coolblossom.lycee.core.args.descriptors;
 
+import static org.junit.Assert.assertEquals;
+
 import java.lang.reflect.Field;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
 
-import net.coolblossom.lycee.core.args.testutil.TestClass;
+import net.coolblossom.lycee.core.args.exceptions.LyceeRuntimeException;
+import net.coolblossom.lycee.core.args.testutil.TestClassCollectionCase;
 
 public class ArrayDescriptorTest {
 
-	@Test
-	public void test_standard_array() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		// 投入するデータ
-		final String[] testData = {"A", "1", "2"};
-
-		// 検証クラス
-		final TestClass testClass = new TestClass();
-
-		// フィールド名
-		final String fieldName = "strAry";
-
-		// フィールド
+	private TestClassCollectionCase makeTestClass(final String fieldName, final String testData[]) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		final TestClassCollectionCase testClass = new TestClassCollectionCase();
 		final Field field = testClass.getClass().getDeclaredField(fieldName);
 		field.setAccessible(true);
-		// フィールドの型
-		final Class<?> fieldType = field.getType();
-		// 配列を構成するクラスの型
-		final Class<?> componentType = fieldType.getComponentType();
-
-		System.out.println("field:"+field.getName());			// field:strAry
-		System.out.println("fieldType:"+fieldType);				// fieldType:class [Ljava.lang.String
-		System.out.println("isArray:" + fieldType.isArray());	// isArray:true
-		System.out.println("componentType:" + componentType);	// componentType:class java.lang.String
-
-		final ArrayDescriptor desc = new ArrayDescriptor(field, componentType);
+		final Class<?> componentType = field.getType().getComponentType();
+		final ArrayDescriptor descriptor = new ArrayDescriptor(field, componentType);
 		for(final String data : testData) {
-			desc.set(testClass, fieldName, data);
+			descriptor.set(testClass, fieldName, data);
 		}
+		return testClass;
+	}
 
-		System.out.println(Stream.of(testClass.strAry).collect(Collectors.joining(",")));
-		// -> A,1,2
+	private Object[] getFieldValue(final TestClassCollectionCase testClass, final String fieldName) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		final Field field = testClass.getClass().getDeclaredField(fieldName);
+		field.setAccessible(true);
+		return (Object[]) field.get(testClass);
+	}
+
+
+	@Test
+	public void test_int_array() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		final String[] testData = {"0", "1", "2"};
+		final TestClassCollectionCase testClass = makeTestClass("argStrArray", testData);
+
+		final String actual = Stream.of(getFieldValue(testClass, "argStrArray"))
+				.map(o-> o.toString())
+				.collect(Collectors.joining(","));
+
+		assertEquals("0,1,2",actual);
+	}
+
+	@Test
+	public void test_string_array() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		final String[] testData = {"A", "1", "2"};
+		final TestClassCollectionCase testClass = makeTestClass("argStrArray", testData);
+
+		final String actual = Stream.of(getFieldValue(testClass, "argStrArray"))
+				.map(o-> o.toString())
+				.collect(Collectors.joining(","));
+
+		assertEquals("A,1,2",actual);
 	}
 
 	@Test
 	public void test_class_array() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		// 投入するデータ
 		final String[] testData = {"A", "1", "2"};
+		final TestClassCollectionCase testClass = makeTestClass("argClsArray", testData);
+		;
+		final String actual = Stream.of(getFieldValue(testClass, "argClsArray"))
+				.map(o-> o.toString())
+				.collect(Collectors.joining(","));
 
-		// 検証クラス
-		final TestClass testClass = new TestClass();
-
-		// フィールド名
-		final String fieldName = "clsAry";
-
-		// フィールド
-		final Field field = testClass.getClass().getDeclaredField(fieldName);
-		field.setAccessible(true);
-		// フィールドの型
-		final Class<?> fieldType = field.getType();
-		// 配列を構成するクラスの型
-		final Class<?> componentType = fieldType.getComponentType();
-
-		System.out.println("field:"+field.getName());			// field:strAry
-		System.out.println("fieldType:"+fieldType);				// fieldType:class [Ljava.lang.String
-		System.out.println("isArray:" + fieldType.isArray());	// isArray:true
-		System.out.println("componentType:" + componentType);	// componentType:class java.lang.String
-
-		final ArrayDescriptor desc = new ArrayDescriptor(field, componentType);
-		for(final String data : testData) {
-			desc.set(testClass, fieldName, data);
-		}
-
-		System.out.println(Stream.of(testClass.clsAry)
-				.map(o -> o.toString())
-				.collect(Collectors.joining(",")));
-		// -> A,1,2
+		assertEquals("A,1,2",actual);
 	}
+
+	@Test(expected=LyceeRuntimeException.class)
+	public void test_verifyField_invalid() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		final String[] testData = {"A", "1", "2"};
+		makeTestClass("argStrList", testData);
+	}
+
 
 }
